50c72af5ad5505211f0b8ea418d1307f0d82aaa5
ZoeChispitas <zoechispitas@gmail.com>
2026-08-24 18:50:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
-def home():
-    return {"status": "ok", "service": "security-demo"}
+def index():
+    return jsonify({"status": "running", "environment": "production"})
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="127.0.0.1", port=5000)

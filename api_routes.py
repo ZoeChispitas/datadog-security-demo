d0f6dc171e70db626c320bbcd10a83308f7fa277
ZoeChispitas <zoechispitas@gmail.com>
2026-08-24 12:31:12 -0400

    Update api_routes.py

--- api_routes.py
+++ api_routes.py
@@ -1,9 +1,5 @@
 import subprocess
-import os
-
-# AWS Secret Key filtrada intencionalmente
-AWS_SECRET_KEY = "AKIAIMNOJVGFDXYZEXAMPLEKEY12345"
 
 def ping_host(host_address):
-    # Vulnerabilidad de inyección de comandos en subprocess
-    subprocess.call("ping -c 1 " + host_address, shell=True)
+    # Ejecución segura pasando argumentos como lista y sin shell=True
+    subprocess.run(["ping", "-c", "1", host_address], check=False)
